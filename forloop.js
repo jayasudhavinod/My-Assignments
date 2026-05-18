32a20ae28595b45f0652d5f6fb7e6d6e48891891
jayasudhavinod <ajs.yogan@gmail.com>
2026-05-18 21:20:44 +0530

    Add files via upload

---
+++ forloop.js
@@ -0,0 +1,17 @@
+let number = 30
+for( let i=0;i < number; i++){
+if ( i%2==1){
+    console.log("The odd number is : "+i)
+}
+
+}
+
+let num = 25
+for (let i=0 ;i <num; i++){
+    if(i%2 ==0){
+        console.log(i + " is even number")
+    }
+    else {
+        console.log(i + " is odd number")
+    }
+}
---
+++ reversestring.js
@@ -0,0 +1,7 @@
+let myname="jayasudha"
+let v=myname.split("")
+for (i=v.length-1;i>=0;i--)
+{
+console.log(v[i])
+}
+   
